8c8b6b1adad1473ab049722891888f905a86bc48
estebanalarcov <estebanalarconv@gmail.com>
2017-08-31 16:31:41 -0300

    funcion get con codigo http arreglado

--- src/main/java/cl/citiaps/spring/backend/rest/FilmService.java
+++ src/main/java/cl/citiaps/spring/backend/rest/FilmService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,11 +46,15 @@ public class FilmService {
 	     return filmRepository.save(resource);
 	}
 	
+
 	@RequestMapping(value = "/{id}/actors", method = RequestMethod.GET)
-	@ResponseBody
-	public List<Actor> getActors(@PathVariable("id") Integer id){
-		this.film=filmRepository.findOne(id);
-		return film.getActors();
-	}
+    public ResponseEntity<List<Actor>> getActors(@PathVariable("id") Integer id){
+        if (filmRepository.exists(id)){
+        	List<Actor> actors = filmRepository.findOne(id).getActors();
+        	return new ResponseEntity<List<Actor>>(actors, HttpStatus.OK);
+        }else{
+        	return new ResponseEntity<List<Actor>>(HttpStatus.NOT_FOUND);
+        }		
+    }
 	 
 }
